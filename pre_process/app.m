b141b9f67ff3fe564415f77540a79034073b0cf0
bh <mzy_martin@hotmail.com>
2017-04-26 17:45:34 +0800

    Reading all .dat Solving exception by closing files MATLAB forgets to close

--- pre_process/app.m
+++ pre_process/app.m
@@ -1,12 +1,5 @@
 %% Setting up
-prefix = '/Users/bh/projects/signal/speech_recog/rec/14307130033-';
-words = {'数字', '语音', '话音', '信号', '分析', '识别', '数据', '中国', '北京', '背景', '上海', '商行', '复旦', '网络', '电脑', 'Speech', 'Voice', 'Sound', 'Happy', 'Lucky', 'Data', 'Recognition', 'File', 'Open', 'Close', 'Start', 'Stop', 'Network', 'Computer', 'China'};
-
-save_to = '/Users/bh/projects/signal/speech_recog/spec/';
-
-FREQ = 8192;
-WIN_LEN = floor(FREQ * 0.02);
-N_OVERLAP = floor(WIN_LEN * 0.5);
+config;
 
 
 %% process
--- pre_process/validation.m
+++ pre_process/validation.m
@@ -1,14 +1,28 @@
 %% find data
-!sh ../find_data.sh
+!sh find_data.sh
+
+%% setting up
+config;
 
 %% read data
-f = fopen('../list.txt', 'r', 'n', 'utf-8');
+f = fopen('list.txt', 'r', 'n', 'utf-8');
 line = fgetl(f);
 count = 0;
 miss = 0;
+SHAPE = [32, 78];
 while ischar(line)
     try
-        wav = audioread(strcat('../', line));
+        wav = audioread(line);
+        
+        spec = spec_squeeze(wav(:,1), 0.4, WIN_LEN, SHAPE(2));
+        try
+            assert(all(size(spec) == SHAPE));   % TODO
+        catch
+            disp(size(spec));
+        end
+        filename = strcat(save_to, char(word), '.txt');
+        save(filename, '-ascii', '-append', 'spec');
+        
         count = count + 1;
     catch
         miss = miss + 1;
@@ -18,10 +32,11 @@ while ischar(line)
         disp(count);
         p = ftell(f);
         fclose('all');      % close files that audioread forgets to close!
-        f = fopen('../list.txt', 'r', 'n', 'utf-8');
+        f = fopen('list.txt', 'r', 'n', 'utf-8');
         fseek(f, p, 0);
     end
 end
-fclose(f);
+fclose('all');
 display(sprintf('Read %d files.', count));
 display(sprintf('Ignored %d files.', miss));
+
